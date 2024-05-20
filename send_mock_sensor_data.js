06d989489b8c6e80bb2d34bc7541d51ee059e388
FrancoBre <bregolif.fb@gmail.com>
2024-05-19 21:56:37 -0300

    feat: apply refactoring and fix video streaming

--- send_mock_sensor_data.js
+++ send_mock_sensor_data.js
@@ -1,69 +1,71 @@
-const fs = require('fs');
-const ffmpeg = require('ffmpeg');
-const sharp = require('sharp');
-const WebSocket = require('ws');
-const pth = require('path');
-const child_process = require('child_process');
+const fs = require("fs");
+const ffmpeg = require("ffmpeg");
+const sharp = require("sharp");
+const WebSocket = require("ws");
+const pth = require("path");
+const child_process = require("child_process");
 
-const ws = new WebSocket('ws://localhost:8999');
+const ws = new WebSocket("ws://localhost:8001");
 
-const videoDir = './video';
-const firstVideoFile = fs.readdirSync(videoDir).find(file => pth.extname(file) === '.mp4');
+const videoDir = "./video";
+const firstVideoFile = fs.readdirSync(videoDir).find((file) =>
+  pth.extname(file) === ".mp4"
+);
 const path = pth.join(videoDir, firstVideoFile);
-const outputTo = 'images';
+const outputTo = "images";
 const fps = 30;
 
 let images = [];
 
 const extractImages = () => {
-    return new Promise((resolve, reject) => {
-        try {
-            new ffmpeg(path, function (err, video) {
-                if (!err) {
-                    video.fnExtractFrameToJPG(outputTo, {
-                        every_n_frames: fps,
-                        file_name: 'image_%t_%s'
-                    }, function (error, files) {
-                        if (error) {
-                            reject(error);
-                        } else {
-                            images = files;
-                            resolve();
-                        }
-                    });
-                } else {
-                    reject(err);
-                }
-            });
-        } catch (err) {
-            reject(err);
+  return new Promise((resolve, reject) => {
+    try {
+      new ffmpeg(path, function (err, video) {
+        if (!err) {
+          video.fnExtractFrameToJPG(outputTo, {
+            every_n_frames: 1, // Extract every frame
+            file_name: "image_%t_%s",
+          }, function (error, files) {
+            if (error) {
+              reject(error);
+            } else {
+              images = files;
+              resolve();
+            }
+          });
+        } else {
+          reject(err);
         }
-    });
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
 };
 
 const sendImages = () => {
-    let index = 0;
-    setInterval(() => {
-        if (index >= images.length) {
-            index = 0; // Reset index to start
-        }
-        const file = images[index];
-        const data = fs.readFileSync(file);
-        ws.send(data);
-        index++;
-    }, 2000); // Send an image every 2 seconds
+  let index = 0;
+  setInterval(() => {
+    if (index >= images.length) {
+      index = 0; // Reset index to start
+    }
+    const file = images[index];
+    const data = fs.readFileSync(file);
+    ws.send(data);
+    index++;
+  }, 1000 / fps);
 };
 
-ws.on('open', async () => {
-    await extractImages();
-    sendImages();
+ws.on("open", async () => {
+  await extractImages();
+  sendImages();
 });
 
-ws.on('error', function error(err) {
-    console.error('WebSocket error:', err);
+ws.on("error", function error(err) {
+  console.error("WebSocket error:", err);
 });
 
-process.on('SIGINT', () => {
-    child_process.execSync('node on_server_shutdown.js');
-    process.exit();
+process.on("SIGINT", () => {
+  child_process.execSync("node on_server_shutdown.js");
+  process.exit();
 });
